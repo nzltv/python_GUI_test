8b815a0efaef07f1b0f754f2fe8cf4a63b93ead1
nzltv <73489886+nzltv@users.noreply.github.com>
2023-06-02 15:44:37 +0300

    Update run_task.bat

--- run_task.bat
+++ run_task.bat
@@ -1,2 +1,2 @@
 cd "C:\Windows\PSTools\"
-psexec -i 16 \\192.168.1.43 -u WIN-K9F18NS5SAF\RBP_PYTHON_01 -p 00000000 cmd /c "python "C:\Jenkins\workspace\run_session\test_pywin.py""
+psexec -i 16 \\192.168.1.43 -u WIN-K9F18NS5SAF\RBP_PYTHON_01 -p 00000000 -accepteula  cmd /c "python "C:\Jenkins\workspace\run_session\test_pywin.py""
