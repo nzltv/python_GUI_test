c3657d8fb76f77b3028a675d1600670a024561ed
nzltv <73489886+nzltv@users.noreply.github.com>
2023-06-02 10:33:11 +0300

    Update test_pywin.py

--- test_pywin.py
+++ test_pywin.py
@@ -1,6 +1,8 @@
 from pywinauto.application import Application
 from time import sleep
 import sys
+import pyautogui
+import os
 
 try:
     app = Application().Start(cmd_line=u'"C:\\Program Files (x86)\\Notepad++\\notepad++.exe" ')
@@ -18,6 +20,9 @@ try:
     Sub = Application().Connect(title=u'Сохранение', class_name='#32770')
     #Sub.Dialog.print_control_identifiers()
     Sub.Dialog.Сохранить.click_input()
+    user = os.getlogin()
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(f'C:\Jenkins\screenshots\{user}.png')
     sleep(10)
     app.Kill_()
 except Exception as e:
