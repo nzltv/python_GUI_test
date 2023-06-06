c765d9a4e986012153f0bdb64377cede3b37d0a2
nzltv <73489886+nzltv@users.noreply.github.com>
2023-06-06 21:02:18 +0300

    Update run_task_2.bat

--- run_task_2.bat
+++ run_task_2.bat
@@ -1,3 +1,3 @@
 cd "C:\Windows\PSTools\"
-for /f "tokens=2 skip=1" %a in ('qwinsta RBP_PYTHON_02') do set "userid=%a"
+for /f "tokens=3 skip=1" %a in ('qwinsta RBP_PYTHON_02') do set "userid=%a"
 psexec -i %userid% \\192.168.1.43 -u WIN-K9F18NS5SAF\RBP_PYTHON_02 -p 00000000 -accepteula  cmd /c "python "C:\Jenkins\workspace\job_RBP_PYTHON_02\test_pywin.py""
