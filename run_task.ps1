78afbd4667230fce8a42f84305497482ca4cd68f
nzltv <73489886+nzltv@users.noreply.github.com>
2023-06-06 21:41:36 +0300

    Delete run_task.ps1

--- run_task.ps1
+++
@@ -1,4 +0,0 @@
-$sessions = quser | Where-Object {$_ -match 'RBP_PYTHON_01'}
-$sessionIds = ($sessions -split ' +')[2]
-cd "C:\Windows\PSTools\"
-.\psexec -i $sessionIds \\192.168.1.43 -u WIN-K9F18NS5SAF\RBP_PYTHON_01 -p 00000000 -accepteula  cmd /c "python "C:\Jenkins\workspace\job_RBP_PYTHON_01\test_pywin.py""
